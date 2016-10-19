145a2634fe435c1b282467487ec8036780d87a6a
mflehmig <martin.flehmig@tu-dresden.de>
2016-10-19 14:55:04 +0200

    Review: header includes, remove namespace std

--- network/include/NetworkPlan.hpp
+++ network/include/NetworkPlan.hpp
@@ -8,16 +8,13 @@
 #ifndef NETWORK_INCLUDE_NETWORKPLAN_HPP_
 #define NETWORK_INCLUDE_NETWORKPLAN_HPP_
 
-#include <memory>
-
-#include <VariableList.hpp>
-#include <SimulationServer.hpp>
-
 #include "Stdafx.hpp"
+#include "SimulationServer.hpp"
 #include "fmi/InputMapping.hpp"
 
 namespace Network
 {
+
     struct NetworkFmuInformation
     {
         size_type simPos;
@@ -29,8 +26,8 @@ namespace Network
 
     struct NetworkPlan
     {
-        std::shared_ptr<NetOff::SimulationServer> server;
-        std::vector<NetworkFmuInformation> fmuNet;
+        shared_ptr<NetOff::SimulationServer> server;
+        vector<NetworkFmuInformation> fmuNet;
     };
 }
 
