03c87786263629a0e2729cd9dfb8632d75e40228
mflehmig <martin.flehmig@tu-dresden.de>
2016-10-19 15:34:34 +0200

    Make the boolean variable a bool

--- src/simulation/SerialSimulation.cpp
+++ src/simulation/SerialSimulation.cpp
@@ -15,7 +15,7 @@ namespace Simulation
 
     void SerialSimulation::simulate()
     {
-        bool_type running;  //MF only set, not used
+        bool running;
         size_type iterationCount = 0;
         const vector<shared_ptr<Solver::ISolver>>& solver = getSolver();
         std::vector<size_type> numSteps(solver.size(),15);
