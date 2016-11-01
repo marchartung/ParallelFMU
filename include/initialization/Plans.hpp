65ed428678b669bf109fc94af92f8f519a909309
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-01 16:21:47 +0100

    Add documentation and refactor name from solvIdToCore to solverIdToCore in order to be more precise

--- include/initialization/Plans.hpp
+++ include/initialization/Plans.hpp
@@ -108,12 +108,34 @@ namespace Initialization
         list<shared_ptr<ConnectionPlan>> inConnections;
     };
 
+    /*! \brief A SchedulePlan holds the mapping of solvers to the physical cores.
+     *
+     * == nodeStructure ==
+     * For a given node i this mapping holds the IDs of the solvers which runs on core j.
+     *          C O R E S
+     *       0   1   2   3
+     *     -------------------->
+     * N 0 | S1  S2  S3
+     * O 1 | S4  S5  S6  S7
+     * D 2 | S8
+     * E 3 | S9  (S10,S11)
+     * S   |
+     *     V
+     * For example, the solvers with ID 10 and 11 run on core 1 of node 3, i.e, nodeStructure[3][1] = {10,11}.
+     *
+     * == solverIdToCore ==
+     * This structure holds for all solvers on which node and core a particular solver runs.
+     * For example, solverIdToCore[0] = (1, 3) means, that solver with ID 0 runs on core 3 of node 1.
+     */
     struct SchedulePlan
     {
-        vector<vector<vector<size_type> > > nodeStructure; // node -> core -> solv
-        vector<tuple<size_type, size_type>> solvIdToCore; // solv -> (node,core)
+        vector<vector<vector<size_type>>> nodeStructure;     ///< node -> core -> solverIDs
+        vector<tuple<size_type, size_type>> solverIdToCore;  ///< solverID -> (node,core)
     };
 
+    /*! \brief A SimulationPlan holds the settings for a particular simulation.
+     *
+     */
     struct SimulationPlan
     {
         string kind;
@@ -128,6 +150,13 @@ namespace Initialization
         DataManagerPlan dataManager;
     };
 
+    /*! \brief A ProgramPlan holds the \ref SimulationPlans for all simulations.
+     *
+     * == simPlans ==
+     * This structure represents the mapping of the simulations to the nodes and threads, i.e., simPlans[nodeNum][threadNum]
+     * holds the \ref SimulationPlan for the simulation which runs on node nodeNum and thread threadNum.
+     *
+     */
     struct ProgramPlan
     {
         //vec2D represents node/thread mapping. simPlans[nodeNum][threadNum]
