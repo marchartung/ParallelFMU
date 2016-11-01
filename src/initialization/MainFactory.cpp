3759b1357c8cf1fad8c27691c4ca71b6be720e2c
mflehmig <martin.flehmig@tu-dresden.de>
2016-11-01 08:17:05 +0100

    Harmonize using clang tidy

--- src/initialization/MainFactory.cpp
+++ src/initialization/MainFactory.cpp
@@ -14,56 +14,80 @@ namespace Initialization
     {
         FMI::AbstractFmu * res;
         if (plan.loader == "fmuSdk")
+        {
             res = new FMI::FmuSdkFmu(plan);
+        }
 #ifdef USE_FMILIB
         else if (plan.loader == "fmiLib")
-        res = new FMI::FmiLibFmu(plan);
+        {
+            res = new FMI::FmiLibFmu(plan);
+        }
 #endif
 #ifdef USE_NETWORK_OFFLOADER
         else if (plan.loader == "network")
+        {
             res = new FMI::EmptyFmu(plan);
+        }
 #endif
         else
+        {
             throw std::runtime_error("MainFactory: Unknown FMU loader type " + plan.loader);
+        }
         return res;
     }
 
-    Synchronization::ConnectionSPtr MainFactory::createConnection(const ConnectionPlan & in, bool outgoing) const
+    Synchronization::ConnectionSPtr MainFactory::createConnection(const ConnectionPlan & in, bool /*outgoing*/) const
     {
         Synchronization::ConnectionSPtr res;
         if (in.kind == "serial")
+        {
             res = Synchronization::ConnectionSPtr(new Synchronization::SerialConnection(in));
+        }
 #ifdef USE_OPENMP
         else if (in.kind == "openmp")
-        res = Synchronization::ConnectionSPtr(new Synchronization::OpenMPConnection(in));
+        {
+            res = Synchronization::ConnectionSPtr(new Synchronization::OpenMPConnection(in));
+        }
 #endif
 #ifdef USE_MPI
         else if (in.kind == "mpi")
+        {
             res = Synchronization::ConnectionSPtr(new Synchronization::MPIConnection(in));
+        }
 #endif
         else
+        {
             throw runtime_error("MainFactory: Unknown connection type " + in.kind);
+        }
         return res;
     }
 
     Simulation::AbstractSimulationSPtr MainFactory::createSimulation(SimulationPlan & in) const
     {
         if (in.kind == "serial")
+        {
             return createSimulationWithKnownType<Simulation::SerialSimulation>(in);
+        }
         //else if(in.kind == "openmp")
         //    return createSimulationWithKnownType<Simulation::OpenMPSimulation>(in);
         else
+        {
             throw runtime_error("MainFactoy: There's no simulation type " + in.kind);
+        }
     }
 
     vector<Synchronization::ConnectionSPtr> MainFactory::createConnectionsOfSolver(const SolverPlan & in) const
     {
         vector<Synchronization::ConnectionSPtr> res;
         for (auto con : in.outConnections)
+        {
             res.push_back(createConnection(*con, true));
+        }
 
         for (auto con : in.inConnections)
+        {
             res.push_back(createConnection(*con, false));
+        }
 
         return res;
     }
